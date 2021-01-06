45a6d1492f7e5a1360ccbe9daa2ab4ec3d2eeb75
Drop-G <tedpetersiv@gmail.com>
2021-01-06 16:19:58 -0500

    middleware route protection installed, User model installed, Dashboard view with logout installed

--- app.js
+++ app.js
@@ -4,16 +4,30 @@ const dotenv = require('dotenv')
 const morgan = require('morgan')
 const exphbs = require('express-handlebars')
 const connectDB = require('./models/db')
+const passport = require('passport')
+const session = require('express-session')
 
 dotenv.config({path: '.env'});
 const app = express()
 connectDB()
+
+require('./middleware/passport')(passport)
+
+app.use(session({
+    secret: 'My secret',
+    resave: false,
+    saveUninitialized: false
+}))
+
+app.use(passport.initialize())
+app.use(passport.session())
 
 if (process.env.NODE_ENV  === 'develpoment'){
     app.use(morgan('dev'))
 }
 // routes
 app.use('/', require('./routes/index'))
+app.use('/auth', require('./routes/auth'))
 
 app.engine('.hbs', exphbs({defaultLayout:'main', extname: '.hbs'}));
 app.set('view engine','.hbs')
